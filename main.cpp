4617d35c0182d43ed2fe62080c49c2c310805c5c
Othalax <dominisiajarzabek@gmail.com>
2026-05-02 21:52:51 +0200

    Bees algorithm finished

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "GeneticAlgorithm.h"
 #include "ProblemLoader.h"
+#include "BeesAlgorithm.h"
 
 int main()
 {
@@ -8,9 +9,9 @@ int main()
 	ProblemData problem_data = problem_loader.loadProblem();
 	Evaluator evaluator(problem_data, problem_data.getDimension());
 
-	int popSize = 100;
-	double crossProb = 0.5;
-	double mutProb = 0.6;
+	/*int popSize = 100;
+	double crossProb = 0.9;
+	double mutProb = 0.35;
 	int iterations = 1000;
 	GeneticAlgorithm ga(popSize, crossProb, mutProb, evaluator);
 	ga.initialize(iterations);
@@ -20,7 +21,22 @@ int main()
 	std::cout << "best solution: ";
 	for (int gene : *best_solution) {
 		std::cout << gene << " ";
-	}
+	}*/
 
+	int popSize = 100;
+	int numEliteSites = 5;
+	int eliteSearchSize = 20;
+	int numSelectedSites = 20;
+	int selectedSearchSize = 10;
+	int iterations = 1000;
+	BeesAlgorithm ba(popSize, numEliteSites, eliteSearchSize, numSelectedSites, selectedSearchSize, evaluator);
+	ba.initialize(iterations);
+	const std::vector<int>* best_solution = ba.getBest();
+	double best_fitness = ba.getBestFitness();
+	std::cout << "final best fitness: " << best_fitness << std::endl;
+	std::cout << "best solution: ";
+	for (int gene : *best_solution) {
+		std::cout << gene << " ";
+	}
 
 }
